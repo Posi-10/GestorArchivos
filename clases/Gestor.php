3f94e264d5ccae80facaa9f0e6065b2a8071fd95
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2021-05-04 11:52:47 -0500

    C_30

--- clases/Gestor.php
+++ clases/Gestor.php
@@ -1,8 +1,24 @@
 <?php
   require_once "Conexion.php";
   class Gestor extends Conexion{
-    public function agregaRegistroArchivo(){
+    public function agregaRegistroArchivo($datos){
       $conexion = Conexion::conectar();
+      $sql = "INSERT INTO t_archivos (id_usuarios,
+                                      id_categoria,
+                                      nombre,
+                                      tipo,
+                                      ruta)
+              VALUES (?, ?, ?, ?, ?)";
+      $query = $conexion->prepare($sql);
+      $query->bind_param("iisss", $datos["id_usuarios"],
+                                  $datos["id_categoria"],
+                                  $datos["nombre"],
+                                  $datos["tipo"],
+                                  $datos["ruta"]
+      );
+      $exito = $query->execute();
+      $query->close();
+      return $exito;
     }
   }
 ?>
--- procesos/gestor/guardarArhivos.php
+++ procesos/gestor/guardarArhivos.php
@@ -1,20 +1,33 @@
 <?php
   session_start();
   require_once "../../clases/Gestor.php";
-  $Gestror = new Gestor();
+  $Gestor = new Gestor();
   $id_categoria = $_POST['categoriasArchivos'];
   $id_usuarios = $_SESSION['id_usuarios'];
   if($_FILES['archivos']['size'] > 0){
+    $carpetaUsuario = "../../archivos/" . $id_usuarios;
+    if(!file_exists($carpetaUsuario)){
+      mkdir($carpetaUsuario, 0777, true);
+    }
     $totalArchivos = count($_FILES['archivos']['name']);
     for($i = 0; $i < $totalArchivos; $i++){
       $nombreArchivo = $_FILES['archivos']['name'][$i];
       $explode = explode(".", $nombreArchivo);
       $tipoArchivo = array_pop($explode);
       $rutaAlmacenamiento = $_FILES['archivos']['tmp_name'][$i];
-      $carpeta = "../../archivos/";
-      $rutaFinal = $carpeta . $nombreArchivo;
-      echo move_uploaded_file($rutaAlmacenamiento, $rutaFinal);
+      $rutaFinal = $carpetaUsuario . "/" . $nombreArchivo;
+      $datosRegistroArchivo = array(
+        "id_usuarios" => $id_usuarios,
+        "id_categoria" => $id_categoria,
+        "nombre" => $nombreArchivo,
+        "tipo" => $tipoArchivo,
+        "ruta" => $rutaFinal
+      );
+      if(move_uploaded_file($rutaAlmacenamiento, $rutaFinal)){
+        $respuesta = $Gestor->agregaRegistroArchivo($datosRegistroArchivo);
+      }
     }
+    echo $respuesta;
   }else{
     echo 0;
   }
