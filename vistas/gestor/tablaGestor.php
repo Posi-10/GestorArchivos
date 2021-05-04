2ab20bd32b35859294c35e5c0886e8a53e762cb6
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2021-05-04 13:51:27 -0500

    C_31

--- vistas/gestor/tablaGestor.php
+++ vistas/gestor/tablaGestor.php
@@ -1,3 +1,26 @@
+<?php
+  session_start();
+  require_once "../../clases/Conexion.php";
+  $conexion = new Conexion();
+  $id_usuarios = $_SESSION['id_usuarios'];
+  $conexion = $conexion->conectar();
+  $sql = "SELECT
+	              archivos.id_archivo AS idArchivo,
+                usuario.nombre AS nombreUsuario,
+                categorias.nombre AS categoria,
+                archivos.nombre AS nombreArchivo,
+                archivos.tipo AS tipoArchivo,
+                archivos.ruta AS rutaArchivo,
+                archivos.fecha AS fecha
+          FROM
+	            t_archivos AS archivos
+	            	INNER JOIN
+	            t_usuarios AS usuario ON archivos.id_usuarios = usuario.id_usuarios
+	            	INNER JOIN
+	            t_categorias AS categorias ON archivos.id_categoria = categorias.id_categoria
+                AND archivos.id_usuarios = '$id_usuarios'";
+  $exito = mysqli_query($conexion, $sql);
+?>
 <div class="row">
   <div class="col-sm-12">
     <div class="table-respomsive">
@@ -12,17 +35,29 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+          <?php
+            while($mostrar = mysqli_fetch_array($exito)){
+              $rutaDescarga = "../archivos/" . $id_usuarios . "/" . $mostrar['nombreArchivo'];
+              $nombreArchivo = $mostrar['nombreArchivo'];
+          ?>
+          <tr style="text-align: center;">
+            <td><?php echo $mostrar['nombreArchivo'];?></td>
+            <td><?php echo $mostrar['tipoArchivo'];?></td>
+            <td>
+              <a href="<?php echo $rutaDescarga; ?>" download="<?php echo $nombreArchivo; ?>" class="btn btn-outline-success btn-sm">
+                <span class="fas fa-download"></span>
+              </a>
+            </td>
             <td></td>
             <td style="text-align: center;">
-            <span type="button" class="btn btn-outline-danger">
+            <span type="button" class="btn btn-outline-danger btn-sm">
               <span class="fas fa-trash"></span>
             </span>
             </td>
           </tr>
+          <?php
+            }  
+          ?>
         </tbody>
       </table>
     </div>
